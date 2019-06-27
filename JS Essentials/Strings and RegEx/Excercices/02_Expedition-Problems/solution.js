8937484e9b588caa1894a9cff6326cb36917d630
Marian <maleksiev12@gmail.com>
2019-06-27 16:24:39 +0300

    Strings and RegEx Exercises

--- JS Essentials/Strings and RegEx/Excercices/02_Expedition-Problems/solution.js
+++ JS Essentials/Strings and RegEx/Excercices/02_Expedition-Problems/solution.js
@@ -1,22 +1,37 @@
 function solve() {
-    let wordInput = document.getElementById('string').value;
-    let textInput = document.getElementById('text').value;
+    let keyword = document.getElementById('string').value;
+    let text = document.getElementById('text').value;
     let result = document.getElementById('result');
 
-    let messageRegex = new RegExp(`${wordInput}(.*)${wordInput}`);
-    let messageMatch = messageRegex.exec(textInput)[1];
-    let locationRegex = /(north|east).*?(\d{2})[^,]*?,[^,]*?(\d{6})/gmi;
+    let messagePattern = new RegExp(`${keyword}(.+)${keyword}`, "gi");
+    let message = `Message: ${messagePattern.exec(text)[1]}`;
 
-    let eastMatch;
-    let northMatch;
-    // let eastMatch = textInput.match(/[Ee][Aa][Ss][Tt] [0-9]{2},\d{6}/);
-    // let northMatch = textInput.match(/[Nn][Oo][Rr][Tt][Hh] [0-9]{2},\d{6}$/);
+    let degreesPattern = /(north|east)[\s\S]*?(\d{2})[^,]*,[^,]*?(\d{6})/gi;
+    let northDegrees;
+    let eastDegrees;
 
-    let match = locationRegex.exec(textInput);
-    console.log(match[0]);
-    // while(match !== null){
-    //
-    // }
-    console.log(messageMatch);
+    let currentDegreesMatch = degreesPattern.exec(text);
 
+    while (currentDegreesMatch) {
+        if (currentDegreesMatch[1].toLowerCase() === 'north') {
+            northDegrees = `${currentDegreesMatch[2]}.${currentDegreesMatch[3]} N`;
+        }
+        else if (currentDegreesMatch[1].toLowerCase() === 'east') {
+            eastDegrees = `${currentDegreesMatch[2]}.${currentDegreesMatch[3]} E`;
+        }
+
+        currentDegreesMatch = degreesPattern.exec(text);
+    }
+
+    let northParagraph = document.createElement('p');
+    northParagraph.textContent = northDegrees;
+    result.appendChild(northParagraph);
+
+    let eastParagraph = document.createElement('p');
+    eastParagraph.textContent = eastDegrees;
+    result.appendChild(eastParagraph);
+
+    let messageParagraph = document.createElement('p');
+    messageParagraph.textContent = message;
+    result.appendChild(messageParagraph);
 }
